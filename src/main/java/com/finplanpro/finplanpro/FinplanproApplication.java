7d446adb17f5bf9954eb52d0c2fa6d2200eedd34
Bunwitchaikun <bosstanasit2546@gmail.com>
2025-11-12 20:38:06 +0700

    chore(d1.1): generate Spring Boot project skeleton (Java17, Boot3.5.7)

---
+++ src/main/java/com/finplanpro/finplanpro/FinplanproApplication.java
@@ -0,0 +1,13 @@
+package com.finplanpro.finplanpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinplanproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinplanproApplication.class, args);
+	}
+
+}
