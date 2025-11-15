cb4c4fd3a7f53fec1268bad827b973abe8b6083f
Bunwitchaikun <bosstanasit2546@gmail.com>
2025-11-15 22:37:40 +0700

    feat(d1.1.11): add /health endpoint and update application configuration

--- src/main/java/com/finplanpro/finplanpro/FinplanproApplication.java
+++ src/main/java/com/finplanpro/finplanpro/FinplanproApplication.java
@@ -2,12 +2,13 @@ package com.finplanpro.finplanpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class FinplanproApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinplanproApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinplanproApplication.class, args);
+    }
 
 }
