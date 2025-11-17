1389609a98c2a7eef309c3bd5447d6a28cfaceb1
Bunwitchaikun <bosstanasit2546@gmail.com>
2025-11-17 20:02:39 +0700

    D2.1.1 Entity: User, Role, UserRole

---
+++ src/main/java/com/finplanpro/finplanpro/entity/User.java
@@ -0,0 +1,38 @@
+package com.finplanpro.finplanpro.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<UserRole> roles;
+
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private UserRole profile;
+}
